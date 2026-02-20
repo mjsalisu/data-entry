330f66a0d13c3458487dbadf497a14e210491620
Jamilu Salisu <jamilusalis@gmail.com>
2026-02-20 11:10:20 +0100

    updated scripts URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycby08gDIgFkPCNVTXD9_jDkyd23EtKfSBSfoElAoxKr-RoBnvk2-IilgtwWpt_DtzFVnWw/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw6S3Jx2ltR8anf9RGRSZ5E4Kqs7_r1Juto4_2k7HiEDAS4N_aWo0Gtg5mAKPZ_0cfa9g/exec";
 const LOCS = {
     "Lagos": ["Ikeja", "Lekki", "Yaba"],
     "Abuja": ["Wuse", "Maitama", "Gwarinpa"]
