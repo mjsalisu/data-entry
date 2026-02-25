2b677fe9acb4811004f6072a091321486546f763
Jamilu Salisu <jamilusalis@gmail.com>
2026-02-25 09:09:02 +0100

    All State Const

--- config.js
+++ config.js
@@ -7,31 +7,119 @@ const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw6S3Jx2ltR8anf9RGRS
  * Add more states here as needed.
  */
 const REFRESHMENTS = {
-    "Benue": {
+    "Adamawa": {
         biscuit: [
-            "Yale Digestive Plus (2 * 65g)",
-            "Yale Digestive ohhh super pack (100g)",
-            "Yale Digestive Choco biscuit (100g)",
-            "Not Provided",
-            "Unticked (left blank)"
+            "McVities Hobnobs 2*78g",
+            "McVities Digestive 76g"
         ],
         drink: [
-            "Grand Malt",
-            "Beta Malt",
-            "Hi Malt",
-            "Dubic Malt",
-            "Not Provided",
-            "Unticked (left blank)"
+            "Maltina"
         ]
     },
-    "Oyo": {
+    "Akwa Ibom": {
         biscuit: [
-            "Rich tea(82g)",
-            "Fab biscuit (100g)",
-            "Mcvities (2*100g)",
-            "Sesamix Biscuit (100g)",
-            "Not Provided",
-            "Unticked (left blank)"
+            "Munchkin Biscuits (120g)",
+            "Fab Biscuit (120g)",
+            "Yale Rich Shortbread (105g)"
+        ],
+        drink: [
+            "Champ Malt",
+            "Beta Malt",
+            "Hi Malt"
+        ]
+    },
+    "Bauchi": {
+        biscuit: [
+            "Nasco Shortcake Biscuit 120g*36",
+            "Mc Vittes Hobb Nobbs OR Mc Vittes Digestive (90g)"
+        ],
+        drink: [
+            "Maltina"
+        ]
+    },
+    "Benue": {
+        biscuit: [
+            "Bees Crunchy Milk and Honey (2*70g)",
+            "Supreme Digestive Biscuit (2*100g)",
+            "Yale Digestive Gold (2*100g)",
+            "Pure Bliss Cookies Milk (54g x 2)"
+        ],
+        drink: [
+            "Maltina",
+            "Grand Malt",
+            "Hi Malt",
+            "Beta Malt"
+        ]
+    },
+    "Delta": {
+        biscuit: [
+            "Munchkin Biscuits (120g)",
+            "Fab Biscuit (120g)",
+            "Yale Rich Shortbread (105g)"
+        ],
+        drink: [
+            "Champ Malt",
+            "Beta Malt",
+            "Hi Malt"
+        ]
+    },
+    "Edo": {
+        biscuit: [
+            "McVities Hobnob (78g)",
+            "McVites Digestive (78g)",
+            "McVites Hobnobs (180g)"
+        ],
+        drink: [
+            "Maltina"
+        ]
+    },
+    "Enugu": {
+        biscuit: [
+            "Bees Crunchy Milk and Honey (2*100g)",
+            "Supreme Digestive Biscuit (2*100g)",
+            "Yale Digestive Gold (2*100g)",
+            "Pure Bliss Cookies Milk (54g*2)"
+        ],
+        drink: [
+            "Maltina",
+            "Grand Malt",
+            "Hi Malt",
+            "Beta Malt"
+        ]
+    },
+    "Kaduna": {
+        biscuit: [
+            "Nasco Shortcake Biscuit (120g*36)"
+        ],
+        drink: [
+            "Maltina"
+        ]
+    },
+    "Kano": {
+        biscuit: [
+            "Nasco Shortcake Biscuit (120g*36)",
+            "Rich Tea (200g)",
+            "Mc Vittes Digestive (78g)",
+            "Mc Vittes Hobnobs (90g)"
+        ],
+        drink: [
+            "Maltina 33CL"
+        ]
+    },
+    "Katsina": {
+        biscuit: [
+            "Nasco Shortcake Biscuit 120g*36"
+        ],
+        drink: [
+            "Maltina"
+        ]
+    },
+    "Lagos": {
+        biscuit: [
+            "Fab Biscuits 100g",
+            "Shortbread 90g",
+            "Rich Tea Biscuit 200g",
+            "NASCO Shortcake 90g"
         ],
         drink: [
             "Malt 33CL",
@@ -40,12 +128,39 @@ const REFRESHMENTS = {
             "Grand Malt"
         ]
     },
-    "Kaduna": {
+    "Ogun": {
         biscuit: [
-            "Nasco Shortcake Biscuit (120g*36)",
+            "McVities Hobnob (78g)",
+            "McVites Digestive (78g)",
+            "McVites Hobnobs (180g)"
+        ],
+        drink: [
+            "Maltina"
+        ]
+    },
+    "Ondo": {
+        biscuit: [
+            "Rich tea (82g)",
+            "Fab Biscuit (100g)",
+            "McVitie's Ginger",
+            "Yale Digestive Gold (2*100g)",
+            "Sesamix"
         ],
         drink: [
             "Maltina",
+            "Beta Maltina",
+            "Height Malt"
+        ]
+    },
+    "Oyo": {
+        biscuit: [
+            "Rich tea(82g)",
+            "Fab biscuit (100g)",
+            "Mcvities (2*100g)",
+            "Sesamix Biscuit (100g)"
+        ],
+        drink: [
+            "Maltina Pet Bottle"
         ]
     }
 };
